9d404ec2ef1b86b050340464671adaa5523fae64
Shangxin Guo <int123c@gmail.com>
2021-04-17 16:28:20 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
     targets: [
         .target(
             name: "Whatever",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [
+                .unsafeFlags(["-suppress-warnings"]),
+            ]
         ),
         .target(
             name: "Example",
